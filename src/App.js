b735aebe7b1431e3dbfe66a29a552e80f6da3bdd
yashjsalian23 <yashjsalian23@gmail.com>
2020-08-02 18:19:22 +0530

    connected with backend

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  async componentDidMount(){
+      const res = await fetch(`${process.env.REACT_APP_DOMAIN}`, {
+        method: 'get'
+      });
+
+      const data = await res.json();
+      console.log(data);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
       </div>
     );
   }
