095f577126da59433db1aabca2d4f03ab0421f04
yashjsalian23 <yashjsalian23@gmail.com>
2020-08-18 20:50:31 +0530

    added navbar and btns

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
+import { Navbar, NavbarBrand, NavbarToggler, Nav, NavItem, Collapse} from 'reactstrap';
 
+import './index.css';
 
-class index extends Component {
-    render() {
-        return (
-            <div>
-                hhfewihweiuhfiuhweufuiwehfiuhewiufhweifhiuwhfieuh
-            </div>
-        );
-    }
-}
+const index = (props) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar light expand="md" >
+                <NavbarBrand>Lorem Ipsum</NavbarBrand>
+                <NavbarToggler style={{color:"#4857B0"}} onClick={toggle} />
+                <Collapse style={{textAlign:'center'}} isOpen={isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                       <NavItem>
+                           <button id="signup-btn">Sign up</button>
+                       </NavItem>
+                       <NavItem>
+                           <button id="signin-btn">Sign in</button>
+                       </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+};
 
 export default index;
