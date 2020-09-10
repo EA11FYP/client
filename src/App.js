921a7f3f6c3ebfa75b9aa605a5dbdeaa099de46c
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-10 11:04:47 +0530

    added header outside route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './pages/Landing';
 import SignupMentor from './pages/signup/Mentor';
 import SignupMentee from './pages/signup/Mentee';
+import Header from './components/header';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="container-fluid">
+          <Header />
           <Switch>
             <Route path="/" exact><Landing /></Route>
             <Route path="/signup/mentor" exact><SignupMentor /></Route>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-import Header from '../components/header';
+//import Header from '../components/header';
 
 class Landing extends Component {
     render() {
         return (
             <div>
-                <Header />
+                {/* <Header /> */}
             </div>
         );
     }
