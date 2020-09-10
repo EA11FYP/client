f817faefe28dfa9d363dd19ad845abff77989663
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-10 11:57:56 +0530

    added responsive style

--- src/pages/signup/Mentor.js
+++ src/pages/signup/Mentor.js
@@ -8,6 +8,16 @@ class Mentor extends Component {
            <React.Fragment>
                 <div className="signup-form-layout">
                     <p className="signup-form-title">MENTOR SIGNUP</p>
+                    <div className="signup-form-inner-layout">
+                        <div>
+                            <label className="signup-form-label">Username: </label><br/>
+                            <input className="signup-form-input" type="text" required autoFocus />
+                        </div>
+                        <div>
+                            <label className="signup-form-label">password: </label><br />
+                            <input className="signup-form-input" type="password" />
+                        </div>
+                    </div>
                 </div>
            </React.Fragment>
         );
