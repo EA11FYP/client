b8466a5e6f8a2ea2df43eb352f2d79b6eb8c39b5
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-10 12:15:18 +0530

    completed styling signup form for web and mobile

--- src/pages/signup/Mentee.js
+++ src/pages/signup/Mentee.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 
+import ButtonSolid from '../../components/UI/button/solid';
+
 import './Styling.css';
 
 class Mentee extends Component {
     render() {
         return (
-            <div>
-                <h1>Mentee form</h1>
-            </div>
+           <React.Fragment>
+                <div className="signup-form-layout">
+                    <p className="signup-form-title">MENTEE SIGNUP</p>
+                    <div className="signup-form-inner-layout">
+                        <form>
+                            <div>
+                                <label className="signup-form-label">Username: </label><br/>
+                                <input className="signup-form-input" type="text" required autoFocus />
+                            </div>
+                            <div>
+                                <label className="signup-form-label">Password: </label><br />
+                                <input className="signup-form-input" type="password" />
+                            </div>
+                            <div>
+                                <label className="signup-form-label">Name: </label><br />
+                                <input className="signup-form-input" type="text" />
+                            </div>
+
+                            <div style={{textAlign:"center", marginTop: 25}}>
+                                <ButtonSolid type="submit" 
+                                style={{background: "#289450",
+                                width:100, 
+                                height: 45, 
+                                fontSize: 18}} > 
+                                    Sign In 
+                                </ButtonSolid>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+           </React.Fragment>
         );
     }
 }
