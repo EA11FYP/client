f9914a87a4e0cd318b379606e21a3fe7e9231263
yashjsalian23 <yashjsalian23@gmail.com>
2021-05-09 21:08:20 +0530

    added chatbot new window

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,17 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-//import Header from '../components/header';
+import ButtonLight from '../components/UI/button/light';
+import './Landing.css';
 
-class Landing extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div>
-                    {/* <Header /> */}
-                </div>
-            </React.Fragment>
-        );
+const Landing = () => {
+
+    let openChatbotHandler = () => {
+        var features = 'directories=no,menubar=no,status=no,titlebar=no,toolbar=no,width=450,height=500';
+        window.open('https://vibrant-swirles-080085.netlify.app/', 'mypopup', features);
     }
-}
+
+    return (
+        <div className="landing container">
+            <div className="landing-chatbot">
+                <p>Interact with our chatbot</p>
+                {/* <button onClick={openChatbotHandler}>Open</button> */}
+
+                <ButtonLight 
+                clicked={openChatbotHandler}
+                style={{
+                width:135, 
+                height: 45, 
+                fontSize: 18,
+               }}>
+                    Chat
+                </ButtonLight>
+            </div>
+        </div>
+    );
+};
 
 export default Landing;
